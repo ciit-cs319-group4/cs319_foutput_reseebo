17024f69d68017345ccaf1e7c6d7bdd2d7b001b5
jcarlo130 <jan.pastor@ciit.edu.ph>
2024-11-24 21:50:50 +0800

    Currency Page

---
+++ lib/currency_page_model.dart
@@ -0,0 +1,10 @@
+import 'currency_page.dart' show CurrencyPageWidget;
+import 'package:flutter/material.dart';
+
+class CurrencyPageModel {
+  
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cs319_foutput_reseebo/currency_page.dart';
 import 'package:cs319_foutput_reseebo/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePageWidget(),
+      home: const CurrencyPageWidget(),
     );
   }
 }
