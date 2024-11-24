ed2720a22d504772f699f77657c8086f55e19571
jcarlo130 <jan.pastor@ciit.edu.ph>
2024-11-25 00:09:01 +0800

    Income and Start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:cs319_foutput_reseebo/currency_page.dart';
 import 'package:cs319_foutput_reseebo/home_page.dart';
+import 'package:cs319_foutput_reseebo/income_page.dart';
+import 'package:cs319_foutput_reseebo/start.dart';
+import 'package:cs319_foutput_reseebo/user_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const CurrencyPageWidget(),
+      home: const IncomePageWidget(),
     );
   }
 }
---
+++ lib/start_model.dart
@@ -0,0 +1,9 @@
+import 'start.dart' show StartWidget;
+import 'package:flutter/material.dart';
+
+class StartModel {
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+}
