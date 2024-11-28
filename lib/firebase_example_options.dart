3b3cc2c5f5dcdfb3c2c805c7c908e1e8ba3f4501
ciit-kojiescolar <129504627+ciit-kojiescolar@users.noreply.github.com>
2024-11-28 20:25:44 +0800

    pushing 3 files just
    edit the files accordingly. Remove the comments before editing.

---
+++ lib/firebase_example_options.dart
@@ -0,0 +1,3 @@
+//Input here the firebase_options.dart in the code snippets provided in ios-code-snippets channel 
+// rename this file as firebase_options.dart and put it under the lib folder lib/firebase_options.dart
+// Remove this comment 
